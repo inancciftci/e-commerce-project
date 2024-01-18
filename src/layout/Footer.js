cc27a15279d35b988c322d57f9327cba6fb70dd9
3nonch <enonch@hotmail.com>
2024-01-18 18:17:12 +0300

    Footer and header layout

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,5 +1,85 @@
+/* eslint-disable jsx-a11y/anchor-has-content */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <>
+      <div className="container flex flex-col mx-auto py-[2rem]">
+        <div className="flex justify-between items-center">
+          <a href="#" className="text-[2.4rem] font-[700] text-[#252B42]">
+            Bandage
+          </a>
+          <div className="flex gap-x-[1.5rem] w-[30rem] justify-end">
+            <a href="#" className="text-[2.4rem]">
+              <i className="fa-brands fa-facebook"></i>
+            </a>
+            <a href="#" className="text-[2.4rem]">
+              <i className="fa-brands fa-instagram"></i>
+            </a>
+            <a href="#" className="text-[2.4rem]">
+              <i className="fa-brands fa-x-twitter"></i>
+            </a>
+          </div>
+        </div>
+        <hr className="my-[3rem]" />
+        <div id="footer-links" className="flex justify-between">
+          <div className="flex flex-col gap-[1rem] flex-1">
+            <h5>Company Info</h5>
+            <a href="#">About Us</a>
+            <a href="#">Carrier</a>
+            <a href="#">We are Hiring!</a>
+            <a href="#">Blog</a>
+          </div>
+          <div className="flex flex-col gap-[1rem] flex-1">
+            <h5>Legal</h5>
+            <a href="#">About Us</a>
+            <a href="#">Carrier</a>
+            <a href="#">We are Hiring!</a>
+            <a href="#">Blog</a>
+          </div>
+          <div className="flex flex-col gap-[1rem] flex-1">
+            <h5>Features</h5>
+            <a href="#">Business Marketing</a>
+            <a href="#">User Analytic</a>
+            <a href="#">Live Chat</a>
+            <a href="#">Unlimited Support</a>
+          </div>
+          <div className="flex flex-col gap-[1rem] flex-1">
+            <h5>Resources</h5>
+            <a href="#">iOS & Android</a>
+            <a href="#">Watch a Demo</a>
+            <a href="#">Customers</a>
+            <a href="#">API</a>
+          </div>
+          <div className="flex flex-col gap-[1rem] flex-[8%]">
+            <h5>Get in Touch</h5>
+            <form className="flex border-black border-solid border-5">
+              <input
+                type="email"
+                placeholder="Your Email"
+                className="p-[1rem] w-[60%] text-[#737373] bg-[#E6E6E6]"
+              />
+              <button
+                type="submit"
+                className="p-[0.5rem] w-[40%] bg-[#23A6F0] text-[white] text-[1.4rem]"
+              >
+                Subscribe
+              </button>
+            </form>
+            <p className="text-[#737373] text-[1.2rem] ">
+              Lorem impsum dolor amit
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="bg-[#FAFAFA]">
+        <div className="container mx-auto">
+          <p className="font-bold text-[#737373] py-[2rem]">
+            Design By: FigmaLand - Coded By: Inanc Ciftci
+          </p>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,5 +1,51 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <>
+      <div className="bg-[#252B42]">
+        <div className="max-sm:hidden container mx-auto py-[1rem] flex justify-between color text-white items-center">
+          <div className="flex gap-x-[3rem]">
+            <button className="flex items-center gap-x-[0.5rem]">
+              <i className="fa-solid fa-phone decoration-white"></i>
+              (225) 555-0118
+            </button>
+            <button className="flex items-center gap-x-[0.5rem]">
+              <i class="fa-regular fa-envelope"></i>
+              michelle.rivera@example.com
+            </button>
+          </div>
+          <div>
+            <h6>Follow us and get a chance to win 80% off!</h6>
+          </div>
+          <div className="flex items-center gap-x-[1rem]">
+            <h6>Follow us : </h6>
+            <div className="flex items-center gap-x-[1rem]">
+              <a href="https://instagram.com" target="_blank" rel="noreferrer">
+                <i class="fa-brands fa-instagram"></i>
+              </a>
+              <a href="https://youtube.com" target="_blank" rel="noreferrer">
+                <i class="fa-brands fa-youtube"></i>
+              </a>
+              <a href="https://facebook.com" target="_blank" rel="noreferrer">
+                <i class="fa-brands fa-facebook"></i>
+              </a>
+              <a href="https://x.com" target="_blank" rel="noreferrer">
+                <i class="fa-brands fa-x-twitter"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container mx-auto py-[1rem] flex">
+        <div className="flex gap-x-[1rem]">
+          <a href="#" className="text-[2.4rem] font-[700]">
+            Bandage
+          </a>
+          <nav></nav>
+        </div>
+      </div>
+    </>
+  );
 }
