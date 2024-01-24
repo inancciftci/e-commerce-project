bee7aaf9a4ea8cd5802c4b495ee46aaded3651f4
3nonch <enonch@hotmail.com>
2024-01-25 00:41:44 +0300

    <Router> added

--- src/layout/PageBody.js
+++ src/layout/PageBody.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom/cjs/react-router-dom.min";
 import HomePage from "../pages/HomePage";
+import AboutPage from "../pages/AboutPage";
+import TeamPage from "../pages/TeamPage";
+import ContactPage from "../pages/ContactPage";
 
 export default function PageBody() {
   return (
     <>
+      {/* Adding router */}
       <Switch>
         <Route path="/" exact>
           <HomePage />
+        </Route>
+        <Route path="/about" exact>
+          <AboutPage />
+        </Route>
+        <Route path="/team" exact>
+          <TeamPage />
+        </Route>
+        <Route path="/contact" exact>
+          <ContactPage />
         </Route>
       </Switch>
     </>
